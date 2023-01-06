5639cb68ab33f1c4cf1c124f40a090103603567e
PeggyHsiao <peggy10346011@gmail.com>
2023-01-07 01:30:20 +0800

    [feature] add list

--- src/components/list.js
+++ src/components/list.js
@@ -1,21 +1,35 @@
 import React, { Component, createRef } from "react";
 
-export class List extends Component {
+export default class List extends Component {
+  inputRef = createRef();
+
   constructor(){
     super()
     this.state = {
       contents: []
     }    
   }
+
   addContent(value){
-    this.setState({ contents: this.state.contents.push(value) })
+      let newList = [...this.state.contents];
+      newList.push(value);
+
+      this.setState({ contents: newList });
+      this.inputRef.current.value = '';
   }
+
   render(){
-      let inputRef = createRef;
+      
       return(
           <div>
-              <input ref={inputRef} type="text" />
-              <button onClick={ () => this.addContent(this.inputRef.value) }>Add</button>
+              <input ref={this.inputRef} type="text" />
+              <button onClick={ () => this.addContent(this.inputRef.current.value) }>Add</button>
+              
+              <ul>
+                  {
+                      this.state.contents.map((item, index) => <li key={index}>{item}</li>)
+                  }
+              </ul>
           </div>
       )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import List from './components/list';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <List />
   </React.StrictMode>
 );
