abddf9f794e22e0a5424834e9052b91159565ec6
Peggy.Hsiao <Peggy.Hsiao@advantech.com.tw>
2023-01-07 09:41:19 +0800

    [feature] delete list

--- src/components/list.js
+++ src/components/list.js
@@ -11,11 +11,20 @@ export default class List extends Component {
   }
 
   addContent(value){
+    if(value === '') return;
+
       let newList = [...this.state.contents];
       newList.push(value);
 
       this.setState({ contents: newList });
       this.inputRef.current.value = '';
+  }
+
+  removeContent(index){
+    let newList = [...this.state.contents];
+    newList.splice(index, 1);
+
+    this.setState({ contents: newList });
   }
 
   render(){
@@ -27,7 +36,9 @@ export default class List extends Component {
               
               <ul>
                   {
-                      this.state.contents.map((item, index) => <li key={index}>{item}</li>)
+                      this.state.contents.map((item, index) => 
+                        <li key={index}>{item}&emsp;<button onClick={ () => { this.removeContent(index) }}>Delete</button></li>
+                      )
                   }
               </ul>
           </div>
