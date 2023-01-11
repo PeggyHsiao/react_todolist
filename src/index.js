29c03f55e8078d7958dbf4df30815af67f1092b2
PeggyHsiao <peggy10346011@gmail.com>
2023-01-11 23:16:46 +0800

    hooks (useContext)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // import ParentAndChild from "./components/ParentAndChild";
-import App from "./components/Hook/useMemo";
+import App from "./components/Hook/useContext";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
